2d4030fa5b5c7c9c9dce2e63140345c0c78bbf27
Patrick Moore <phatrickmoore@gmail.com>
2016-11-20 22:17:47 -0600

    improved ordering of imported code splicing. This assumes the import structure is at least a DAG, and appends the eldest import at the very front of the file. seems to work ok!

--- Lcombinator.py
+++ Lcombinator.py
@@ -1,7 +1,7 @@
 import argparse
 import re
 
-def get_imports_and_raw_code(l_file):
+def get_imports(l_file):
     full_prog = ''
     with open(l_file, 'r') as prog:
         for line in prog:
@@ -14,30 +14,53 @@ def get_imports_and_raw_code(l_file):
     required_L_files = set()
     for f in re.finditer(import_regex, stringless_prog):
         required_L_files.add(f.group('file'))
+    
+    return required_L_files
 
+def build_import_graph(lib_dir, l_file):
+    import_order = []
+    currents = get_imports(lib_dir + '/' + l_file)
+    nexts = set()
+    while len(currents) > 0:
+        for f in currents:
+            nexts = nexts | get_imports(lib_dir + '/' + f + '.L')
+        import_order.append(set.copy(currents))
+        currents = set.copy(nexts)
+        nexts = set()
+    return import_order
+
+def collapse_imports(imports):
+    all_imports = set()
+    for i in reversed(range(len(imports))):
+        imports[i] = imports[i] - all_imports
+        all_imports = all_imports | imports[i]
+    return imports
+
+def get_raw_code(l_file):
+    full_prog = ''
+    with open(l_file, 'r') as prog:
+        for line in prog:
+            full_prog += line
+
+    import_regex = '(?:import\s+)(?P<file>\w+)'
     import_regex += '\s+in\s+'
     importless_prog = re.sub(import_regex, '', full_prog)
 
-    return required_L_files, importless_prog
+    return importless_prog
 
-def combinate(l_file):
-    full_import_code = ''
-    next_imports, raw_code = get_imports_and_raw_code(l_file)
-    imports = set()
-    while len(next_imports) > 0:
-        new_imports = set()
-        for import_file in next_imports:
-            import_file_imports, import_file_code = get_imports_and_raw_code(import_file + '.L')
-            full_import_code += import_file_code + '\n'
-            new_imports = new_imports | import_file_imports
-        imports = imports | next_imports
-        next_imports = new_imports - imports
-    return imports, full_import_code + raw_code
+def combinate(lib_dir, l_file):
+    final_prog = ''
+    final_order = reversed(collapse_imports(build_import_graph(lib_dir, l_file)))
+    for prog_set in final_order:
+        for prog in prog_set:
+            final_prog += get_raw_code(lib_dir + '/' + prog + '.L')
+    return final_prog + get_raw_code(lib_dir + '/' + l_file)
 
 parser = argparse.ArgumentParser()
+parser.add_argument('lib_dir', metavar = 'LIB_DIR', type=str)
 parser.add_argument('program', metavar='PROG', type=str)
 
 args = parser.parse_args()
-imports, full_import_code = combinate(args.program)
-print(imports)
-print(full_import_code)
+full_code = combinate(args.lib_dir, args.program)
+print(full_code)
+
