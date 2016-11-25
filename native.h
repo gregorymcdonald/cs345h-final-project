303d9e1133cc5bb157d0b57e8db9f5fda6b3759d
MathSquared <MathSquared@users.noreply.github.com>
2016-11-25 16:04:45 -0600

    Added warning about incompatible ABIs

--- native.h
+++ native.h
@@ -6,5 +6,9 @@ typedef void* shlib;
 //
 // The function is pulled from the file "libmodule.so" in the working directory, replacing module with the value of the module parameter.
 // Throws a runtime_error if the module or function cannot be found.
+//
+// WARNING: Your method call may fail spectacularly unless all of the ArgTypes and the RetType are standard layout types.
+// http://en.cppreference.com/w/cpp/concept/StandardLayoutType
+// In particular, references (like int&) and standard library types (like std::vector) are NOT standard layout types.
 template <typename RetType, typename... ArgType>
 RetType call_native(std::string module, std::string function, ArgType... argv);
